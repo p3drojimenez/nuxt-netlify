8b4c03b4e9a6bdc45d3854d15aadea242262380f
Pedro <pedrojimenezhontanilla@gmail.com>
2019-07-10 13:19:39 +0200

    testing bacco api

--- pages/index.vue
+++ pages/index.vue
@@ -1,69 +1,24 @@
 <template>
   <v-layout
-    column
     justify-center
     align-center
   >
     <v-flex
       xs12
-      sm8
+      sm6
       md6
     >
-      <div class="text-xs-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template New proyect!
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >documentation</a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >discord</a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >issue board</a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >Nuxt Documentation</a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >Nuxt GitHub</a>
-        </v-card-text>
+      <v-card v-for="exp in data.data">
+        <v-img
+          :src="exp.file"
+          aspect-ratio="2.75"
+        ></v-img>
+        <p>{{ exp.name }}</p>
+        <p>{{ exp.city.name }} </p>
+        <p>{{ exp.langs }}</p>
         <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            flat
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
+          <v-btn flat color="orange">Share</v-btn>
+          <v-btn flat color="orange">Explore</v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
@@ -71,13 +26,18 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
+import axios from 'axios'
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+  },
+  data() {
+    return {
+      data: ''
+    }
+  },
+  mounted() {
+    axios.get('http://api.baccoexperiences.com/api/publics/experiences')
+    .then(response => (this.data = response))
   }
 }
 </script>
